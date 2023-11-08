78dea3b33bb6dbd146b0e339947a86dacc039647
Joakim Hellsén <tlovinator@gmail.com>
2023-11-08 23:32:38 +0100

    Remove CSV exporter

--- tests/test_panso.py
+++ tests/test_panso.py
@@ -1,7 +1,6 @@
 import json
 from typing import TYPE_CHECKING
 
-from sitemapparser.exporters.csv_exporter import CSVExporter
 from sitemapparser.exporters.json_exporter import JSONExporter
 from sitemapparser.site_map_parser import SiteMapParser
 
@@ -20,15 +19,7 @@ def test_panso() -> None:
         urls: UrlSet = sm.get_urls()
         assert urls is not None
 
-    csv_exporter = CSVExporter(sm)  # type: ignore  # noqa: PGH003
-    if sm.has_sitemaps():
-        sitemaps_csv: str = csv_exporter.export_sitemaps()
-        assert sitemaps_csv is not None
-    elif sm.has_urls():
-        urls_csv: str = csv_exporter.export_urls()
-        assert urls_csv is not None
-
-    json_exporter = JSONExporter(sm)  # type: ignore  # noqa: PGH003
+    json_exporter = JSONExporter(sm)
     if sm.has_sitemaps():
         sitemaps_json: str = json_exporter.export_sitemaps()
         assert sitemaps_json is not None
